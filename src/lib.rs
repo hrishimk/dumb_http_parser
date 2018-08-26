14182f53953d4bea60b5da302a04aa6173aed700
hrishi <hrishikesh.mk1@gmail.com>
2018-08-26 17:57:24 +0530

    Added data check before parse

--- src/lib.rs
+++ src/lib.rs
@@ -100,21 +100,26 @@ impl<'a> HttpParser<'a> {
                 if *n == b' ' && !cur_line_first_space_set {
                     cur_line_first_space_pos = i;
                     cur_line_first_space_set = true;
-                    cur_line_last_header_key = [cur_line_start_pos, i - 2];
+                    cur_line_last_header_key = [cur_line_start_pos, i - 1];
                 } else {
                     if *n == b'\r' {
-                        if b"Cookie:"
-                            == &self.buf[cur_line_last_header_key[0]..cur_line_last_header_key[1]]
-                        {
-                            self.set_cookie([cur_line_first_space_pos, i - 1]);
-                        } else if b"Content-Length:"
-                            == &self.buf[cur_line_last_header_key[0]..cur_line_last_header_key[1]]
-                        {
-                            self.set_content_length([cur_line_first_space_pos, i - 1]);
-                        } else if b"Content-Type:"
-                            == &self.buf[cur_line_last_header_key[0]..cur_line_last_header_key[1]]
-                        {
-                            self.set_content_type([cur_line_first_space_pos, i - 1]);
+                        let key = str::from_utf8(
+                            &self.buf[cur_line_last_header_key[0]..cur_line_last_header_key[1]],
+                        );
+
+                        let key = match key {
+                            Ok(x) => x.to_lowercase(),
+                            Err(_) => "".to_string(),
+                        };
+
+                        let key = key.as_str();
+
+                        if "cookie" == key {
+                            self.set_cookie([cur_line_first_space_pos + 1, i]);
+                        } else if "content-length" == key {
+                            self.set_content_length([cur_line_first_space_pos + 1, i]);
+                        } else if "content-type" == key {
+                            self.set_content_type([cur_line_first_space_pos + 1, i]);
                         }
 
                         cur_line_last_was_r = true;
@@ -150,6 +155,13 @@ impl<'a> HttpParser<'a> {
 
     pub fn get_method(&self) -> &HttpMethod {
         &self.method
+    }
+
+    pub fn get_content_length(&self) -> usize {
+        str::from_utf8(&self.buf[self.content_length[0]..self.content_length[1]])
+            .unwrap_or("0")
+            .parse::<usize>()
+            .unwrap_or(0)
     }
 
     pub fn get_src(&self) -> &str {
@@ -195,8 +207,11 @@ impl<'a> HttpParser<'a> {
 
         if nature == "params" {
             params_str = self.get_params().split(splitter);
-        } else {
+        } else if nature == "cookies" {
             params_str = self.get_cookie().split(splitter);
+        } else {
+            println!("body is {:?}", self.get_body());
+            params_str = self.get_body().split(splitter);
         }
         for pair in params_str {
             let key_val: Vec<&str> = pair.split('=').collect();
@@ -214,8 +229,122 @@ impl<'a> HttpParser<'a> {
         self.get_map("params", "&")
     }
 
+    pub fn get_body_map(&self) -> HashMap<&str, &str> {
+        self.get_map("body", "&")
+    }
+
     pub fn get_cookie_map(&self) -> HashMap<&str, &str> {
         self.get_map("cookies", "; ")
+    }
+
+    pub fn is_body_read(&self) -> bool {
+        if (self.body[1] - self.body[0]) < self.get_content_length() {
+            false
+        } else {
+            true
+        }
+    }
+
+    pub fn get_body_remain(&self) -> usize {
+        self.get_content_length() - self.body[1]
+    }
+
+    pub fn check_data(buf: &[u8]) -> usize {
+        let iter = buf.iter();
+
+        let mut end_checker: [u8; 4];
+
+        let end = 0;
+
+        let mut cur_pos = 0;
+
+        let mut cur_line_start = false;
+        let mut cur_line = 0;
+        let mut cur_line_start_pos = 0;
+        let mut cur_line_key_set = false;
+        let mut cur_line_first_space_set = false;
+        let mut cur_line_first_space_pos = 0;
+        let mut cur_line_last_was_r = false;
+
+        let mut cur_line_last_header_key: [usize; 2] = [0, 0];
+
+        let mut content_len = 0;
+        let mut body_len = 0;
+        let mut body_begin = 0;
+
+        for (i, n) in iter.enumerate() {
+            cur_pos = i;
+
+            if cur_line_start {
+                if *n == b'\r' {
+                    let len = buf.len();
+                    println!("i+2 is {}", i + 2);
+                    println!("len is {}", len);
+                    if (i + 2 > len - 1) {
+                        body_len = 0;
+                    } else {
+                        body_len = len - i - 2;
+                        body_begin = i + 2;
+                    }
+                    break;
+                }
+            }
+
+            if cur_line_last_was_r {
+                cur_line_start = true;
+                cur_line += 1;
+                cur_line_start_pos = i + 1;
+                cur_line_key_set = false;
+                cur_line_first_space_set = false;
+                cur_line_first_space_pos = i;
+                cur_line_last_was_r = false;
+                cur_line_last_header_key = [0, 0];
+                continue;
+            } else {
+                cur_line_start = false;
+            }
+
+            if cur_line != 0 {
+                if *n == b' ' && !cur_line_first_space_set {
+                    cur_line_first_space_pos = i;
+                    cur_line_first_space_set = true;
+                    cur_line_last_header_key = [cur_line_start_pos, i - 1];
+                } else {
+                    if *n == b'\r' {
+                        let key = str::from_utf8(
+                            &buf[cur_line_last_header_key[0]..cur_line_last_header_key[1]],
+                        );
+
+                        let key = match key {
+                            Ok(x) => x.to_lowercase(),
+                            Err(_) => "".to_string(),
+                        };
+
+                        let key = key.as_str();
+
+                        if "content-length" == key {
+                            //content_len([cur_line_first_space_pos + 1, i]);
+                            content_len = str::from_utf8(&buf[cur_line_first_space_pos + 1..i])
+                                .unwrap_or("0")
+                                .parse::<usize>()
+                                .unwrap_or(0);
+                        }
+
+                        cur_line_last_was_r = true;
+                    }
+                }
+            } else {
+                //println!("line is zero");
+                //Check first line
+                if *n == b'\r' {
+                    cur_line_last_was_r = true;
+                }
+            }
+        }
+
+        //println!("body_len is {}", body_len);
+
+        content_len - body_len
     }
 }
 
@@ -256,4 +385,28 @@ mod tests {
         test_map.insert("active", "1");
         assert_eq!(test_map, parser.get_params_map());
     }
+
+    #[test]
+    fn get_content_length_works() {
+        let a = b"GET /get?type=dbs&active=1 HTTP/1.1\r\nContent-Length: 100\r\n\r\n";
+        let mut parser = HttpParser::new(a);
+        parser.parse();
+
+        println!("parser is {:#?}", parser);
+        assert_eq!(100, parser.get_content_length());
+    }
+    #[test]
+    fn check_data_checker_half() {
+        let a = b"GET /get?type=dbs&active=1 HTTP/1.1\r\nContent-Length: 8\r\n\r\nabcdef";
+
+        assert_ne!(HttpParser::check_data(a), 0);
+    }
+
+    #[test]
+    fn check_data_checker_full() {
+        let a = b"GET /get?type=dbs&active=1 HTTP/1.1\r\nContent-Length: 6\r\n\r\nabcdef";
+
+        assert_eq!(HttpParser::check_data(a), 0);
+    }
+
 }
